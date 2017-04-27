28e0ab8d4351e263f659b04ea4908e43ec78be09
Edmilson <edmilsonrobson@gmail.com>
2017-04-27 13:34:24 -0300

    Removed countdown references

--- maincontroller.js
+++ maincontroller.js
@@ -1,28 +1,17 @@
 (function(){
 
     var app = angular.module("GithubViewer");
-    app.controller('MainController', function($scope, $interval, $location) {
-
-        var decrementCountdown = function(){
-            $scope.countdown -= 1;
-            if ($scope.countdown <= 0){
-                $scope.getUserInfo($scope.username);
-            }
-        };
+    app.controller('MainController', function($scope, $location) {
 
 
         $scope.getUserInfo = function(name){
             $location.path("/user/" + name);
         };
 
-        var startCountdown = function(){
-            $interval(decrementCountdown, 1000, $scope.countdown);
-        };
+
 
         $scope.username = "edmilsonrobson";
-        $scope.countdown = 5;
 
-        startCountdown();
 
     });
 })();
